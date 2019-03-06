1389fc7d70efcd45a9f61ce663500b08b743211e
isoppp <prog@d2deck.com>
2019-03-06 13:56:44 +0900

    Fixed auth bug

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,6 +8,27 @@ from flaskr.db import get_db
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+
+
 @bp.route("/register", methods=("GET", "POST"))
 def register():
     if request.method == "POST":
@@ -49,38 +70,17 @@ def login():
         elif not check_password_hash(user["password"], password):
             error = "Incorrect password."
 
-            if error is None:
-                session.clear()
-                session["user_id"] = user["id"]
-                return redirect(url_for("index"))
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
 
         flash(error)
 
     return render_template("auth/login.html")
 
 
-@bp.before_request
-def load_logged_in_user():
-    user_id = session.get("user_id")
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,))
-
-
 @bp.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("index"))
-
-
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for("auth.login"))
-
-        return view(**kwargs)
-
-    return wrapped_view()
